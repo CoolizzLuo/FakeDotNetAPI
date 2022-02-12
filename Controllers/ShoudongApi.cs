369599aec783604f19b0b15839a6309100a2cac9
Enzo <duenzo1010@gmail.com>
2022-02-12 13:35:29 +0800

    2-6

---
+++ Controllers/ShoudongApi.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FakeDotNetAPI.Controllers
+{
+    [Route("api/shoudongapi")]
+    [ApiController]
+    public class ShoudongApiController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            
+            return new string[] { "s1", "s2" };
+        }
+    }
+}
