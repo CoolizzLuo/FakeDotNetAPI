82f362e76c6c11b3b24101c657e8beb10f2f3262
Enzo <duenzo1010@gmail.com>
2022-02-13 04:00:54 +0800

    feat: init Database seed (3-9)

--- Database/AppDbContext.cs
+++ Database/AppDbContext.cs
@@ -1,5 +1,10 @@
-﻿using FakeDotNetAPI.Models;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using FakeDotNetAPI.Models;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 
 namespace FakeDotNetAPI.Database
 {
@@ -12,5 +17,28 @@ namespace FakeDotNetAPI.Database
         
         public DbSet<TouristRoute> TouristRoutes { get; set; }
         public DbSet<TouristRoutePicture> TouristRoutePictures { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<TouristRoute>().HasData(new TouristRoute()
+            //{
+            //    Id = Guid.NewGuid(),
+            //    Title = "ceshititle",
+            //    Description = "shuoming",
+            //    OriginalPrice = 0,
+            //    CreateTime = DateTime.UtcNow
+            //});
+            
+            var touristRouteJsonData = File.ReadAllText(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"/Database/touristRoutesMockData.json");
+            IList<TouristRoute> touristRoutes = JsonConvert.DeserializeObject<IList<TouristRoute>>(touristRouteJsonData);
+            modelBuilder.Entity<TouristRoute>().HasData(touristRoutes);
+
+            var touristRoutePictureJsonData = File.ReadAllText(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"/Database/touristRoutePicturesMockData.json");
+            IList<TouristRoutePicture> touristRoutePictures = JsonConvert.DeserializeObject<IList<TouristRoutePicture>>(touristRoutePictureJsonData);
+            modelBuilder.Entity<TouristRoutePicture>().HasData(touristRoutePictures);
+
+            
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
--- Migrations/20220212184455_DataSeeding.Designer.cs
+++ Migrations/20220212184455_DataSeeding.Designer.cs
@@ -4,14 +4,16 @@ using FakeDotNetAPI.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace FakeDotNetAPI.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220212184455_DataSeeding")]
+    partial class DataSeeding
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
